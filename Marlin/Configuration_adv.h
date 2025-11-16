c56b5aced486cad23480da808dd4951da47585ed
Oleksandr Shyshatskyi <shalal545@gmail.com>
2025-11-16 12:13:14 +0000

    Increase lcd timeout

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1650,7 +1650,7 @@
   #endif
 
   // The timeout to return to the status screen from sub-menus
-  //#define LCD_TIMEOUT_TO_STATUS 15000   // (ms)
+  #define LCD_TIMEOUT_TO_STATUS 300000   // (ms)
 
   // Scroll a longer status message into view
   //#define STATUS_MESSAGE_SCROLLING
